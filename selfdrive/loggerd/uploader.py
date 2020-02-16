ce7bf19e9622232d285624b4372c1e9d5761c1f7
slavka89 <51024690+slavka89@users.noreply.github.com>
2020-02-15 19:49:55 -0800

    Update uploader.py

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -76,19 +76,6 @@ def is_on_wifi():
     cloudlog.exception("is_on_wifi failed")
     return False
 
-def is_on_hotspot():
-  try:
-    result = subprocess.check_output(["ifconfig", "wlan0"], stderr=subprocess.STDOUT, encoding='utf8')
-    result = re.findall(r"inet addr:((\d+\.){3}\d+)", result)[0][0]
-
-    is_android = result.startswith('192.168.43.')
-    is_ios = result.startswith('172.20.10.')
-    is_entune = result.startswith('10.0.2.')
-
-    return (is_android or is_ios or is_entune)
-  except:
-    return False
-
 class Uploader():
   def __init__(self, dongle_id, root):
     self.dongle_id = dongle_id
